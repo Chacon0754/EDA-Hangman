16f40f62c376dce23e2554531569a477febaaf07
debian-mocos <130593875+MoXcz@users.noreply.github.com>
2024-11-03 18:10:13 -0500

    Add formatting and one of missing authors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Hangman Game
 # Autores:
 # Martin Eduardo Chacon Orduño - 351840
+# Oscar Joaquin Marquez Ortega - 367726
 #
 # 30/09/2024
 
@@ -22,7 +23,7 @@ pygame.display.set_caption("Hangman Game")
 # button variables
 RADIUS = 20
 GAP = 15
-letters = [] 
+letters = []
 startX = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
 startY = 400
 A = 65
@@ -34,9 +35,9 @@ for i in range(26):
     letters.append([x, y, chr(A + i), True])
 
 # fonts
-LETTERFONT = pygame.font.SysFont('comicsans', 40)
-WORDFONT = pygame.font.SysFont('comicsans', 45)
-TITLEFONT = pygame.font.SysFont('comicsans', 70)
+LETTERFONT = pygame.font.SysFont("comicsans", 40)
+WORDFONT = pygame.font.SysFont("comicsans", 45)
+TITLEFONT = pygame.font.SysFont("comicsans", 70)
 
 # images
 images = []
@@ -44,10 +45,11 @@ for i in range(8):
     imgUrl = "img\hangman" + str(i) + ".png"
     image = pygame.image.load(imgUrl)
 
-    scaledImage = pygame.transform.scale(image, (400, int(image.get_height() * (200 / image.get_width()))))
+    scaledImage = pygame.transform.scale(
+        image, (400, int(image.get_height() * (200 / image.get_width())))
+    )
     images.append(scaledImage)
 backgroundColor = images[0].get_at((10, 10))
-
 
 
 # game variables
@@ -59,7 +61,6 @@ guessed = []
 # colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-
 
 
 def draw():
@@ -74,14 +75,14 @@ def draw():
         if letter in guessed:
             displayWord += letter + " "
         else:
-            displayWord +="_ "
+            displayWord += "_ "
     text = WORDFONT.render(displayWord, 1, BLACK)
     window.blit(text, (400, 200))
 
     # draw buttons
     for letter in letters:
         x, y, ltr, visible = letter
-        if visible: 
+        if visible:
             pygame.draw.circle(window, BLACK, (x, y), RADIUS, 3)
             text = LETTERFONT.render(ltr, 1, BLACK)
             window.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
@@ -89,25 +90,28 @@ def draw():
     window.blit(images[hangmanStatus], (0, 120))
     pygame.display.update()
 
+
 def displayMessage(message):
     pygame.time.delay(1000)
     window.fill(backgroundColor)
     text = WORDFONT.render(message, 1, BLACK)
-    window.blit(text, (WIDTH / 2 - text.get_width(), HEIGHT  / 2 - text.get_height() / 2))
+    window.blit(
+        text, (WIDTH / 2 - text.get_width(), HEIGHT / 2 - text.get_height() / 2)
+    )
     pygame.display.update()
     pygame.time.delay(3000)
+
 
 def main():
     # game loop variables
     FPS = 60
     clock = pygame.time.Clock()
-    run  = True
+    run = True
 
     while run:
         global hangmanStatus
         clock.tick(FPS)
 
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -117,7 +121,7 @@ def main():
                 for letter in letters:
                     x, y, ltr, visible = letter
                     if visible:
-                        dis = math.sqrt((x - mX)**2 + (y - mY)**2)
+                        dis = math.sqrt((x - mX) ** 2 + (y - mY) ** 2)
                         if dis < RADIUS:
                             letter[3] = False
                             guessed.append(ltr)
@@ -134,9 +138,9 @@ def main():
                             guessed.append(ltr)
                             if ltr not in word:
                                 hangmanStatus += 1
-        
+
         draw()
-        
+
         won = True
         for letter in word:
             if letter not in guessed:
@@ -148,7 +152,7 @@ def main():
         if hangmanStatus == 7:
             displayMessage("You LOST!!")
             break
-            
+
 
 main()
 pygame.quit()
